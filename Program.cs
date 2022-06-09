624aca849c8fb81ad3b414d21560238cb76a4c86
MikhailVinogradov224 <m89119750476@gmail.com>
2022-06-09 21:45:34 +0300

    done

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int N, M, sum=0;
+Console.Write("Введите N: ");
+N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите M: ");
+M = Convert.ToInt16(Console.ReadLine());
+for (int i = N; i <= M; i++)
+    sum += i;
+Console.WriteLine(sum);
